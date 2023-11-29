a8e8b41dead8dd2a838d63f1c4936121deabc055
nathabee <nathalie.werner.fr@gmail.com>
2023-11-29 16:22:22 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='see https://c17hawke.github.io/wafer_mlops_docs/stage1_init_setup/',
+    author='nathabee',
+    license='MIT',
+)
